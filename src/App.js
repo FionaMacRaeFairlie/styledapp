9da2a1d0932235d2b1a911b5d4ca7ed625f73a03
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-10-13 17:40:57 +0100

    updated tutorial exercise using functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+// import Navigation from "./nav";
+import "./custom.scss";
+//import "./theme.scss";
+//import "./mixin.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container class="container-fluid">
+    <Card> 
+      <Card.Header>
+          <strong className="mr-auto">Section 1</strong>
+        </Card.Header>
+        <Card.Body>This makes use of Bootstrap to create a responsive layout</Card.Body>
+        </Card>
+    <Card>
+        <Card.Header>
+          <strong className="mr-auto">Section 2</strong>
+        </Card.Header>
+        <Card.Body>If you are unfamiliar with Bootstrap styles w3schools has some introductory material.</Card.Body>
+      </Card>
+    <Card>
+        <Card.Header>
+          <strong className="mr-auto">Section 3</strong>
+        </Card.Header>
+        <Card.Body> <a href ="https://github.com/react-bootstrap/code-sandbox-examples"> https://github.com/react-bootstrap/code-sandbox-examples </a> has more information about how to use Bootstrap with React. </Card.Body>
+      </Card>
+ 
+      <Button> Click me</Button>
+  </Container>
   );
 }
 
+// function App() {
+//   const [theme, setTheme] = useState("theme-1");
+  
+//    const handleChange = () =>{
+//     if (theme == "theme-1") {
+//       setTheme("theme-2");}
+//     else {
+//       setTheme("theme-1");
+//     }
+//   }
+
+//   return (
+//     <div className={theme}>
+//       <div className="container">
+//         <div className="left">
+//           <Navigation />
+//         </div>
+//         <div className="right">
+//           <button className="button" onClick={handleChange}>
+//             Change color
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 export default App;
